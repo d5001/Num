50e62a2ae5a83e32b1bb7be23fb9d3ba6deea4d1
d5001 <81365403+d5001@users.noreply.github.com>
2021-10-17 15:04:49 +0800

    Update Num.java

--- Num.java
+++ Num.java
@@ -1,26 +1,36 @@
 package me.mcdcs.rgbtext;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Num {
 
     private String num;
 
     private String[] zm = new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","s","y","z"};
+    private ArrayList<String> il = new ArrayList<>();
 
     public Num(String num){
         this.num = num;
+        il.addAll(Arrays.asList(zm));
     }
-    
+
     public String sze(int jz){//十进制转任意进制
         StringBuilder n = new StringBuilder();
         int i = Integer.parseInt(num);
         while (true){
-            int y = i % jz;
+            int y;
+            if (i < jz){
+                y = i;
+            }else {
+                y = i % jz;
+            }
             if (y >= 10){
-                n.insert(0,zm[y-10]);
+                n.insert(0,il.get(y-10));
             }else {
                 n.insert(0, y);
             }
-            if ((double) i / jz == (double) 1 / jz){
+            if ((double) i / jz == (double) 1 / jz | i < jz){
                 break;
             }
             i = (i - y) / jz;
@@ -40,15 +50,10 @@ public class Num {
                 i = i * jz;
                 b++;
             }
-            int p = 0;
-            for (String k : zm){
-                if (k.equalsIgnoreCase(s)){
-                    p += 10;
-                    break;
-                }
-                p++;
-            }
-            if (p < 10){
+            int p;
+            if (il.contains(s)){
+                p = il.indexOf(s) + 10;
+            }else {
                 p = Integer.parseInt(s);
             }
             n += i * p;
@@ -56,7 +61,7 @@ public class Num {
         }
         return String.valueOf(n);
     }
-    
+
     public String getNum(){
         return num;
     }
